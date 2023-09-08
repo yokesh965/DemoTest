7390feb0889c9a0a7c48980811a72531fb2eed6b
Yokesh K <64349408+yokesh965@users.noreply.github.com>
2023-09-08 17:53:22 +0530

    Update BaseTest.java

--- src/test/java/com/DemoWebShop/TestScripts/BaseTest.java
+++ src/test/java/com/DemoWebShop/TestScripts/BaseTest.java
@@ -1,8 +1,6 @@
 package com.DemoWebShop.TestScripts;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -11,20 +9,9 @@ public class BaseTest {
 	WebDriver driver;
 
 	@BeforeTest
-	public void LaunchApplication() {
-		String os = System.getProperty("os.name").toLowerCase();
-		if (os.contains("win")) {
-			driver = new ChromeDriver();
-		}
-
-		else if (os.contains("nix") || os.contains("aix") || os.contains("nux")) {
-			ChromeOptions options = new ChromeOptions();
-			options.addArguments("--headless");
-			driver = new ChromeDriver(options);
-		}
-		
+	public void LaunchApplication() {		
 		// 1. Open the browser
-		//driver = new EdgeDriver();
+		driver = new EdgeDriver();
 
 		// 2. Maximize it
 		driver.manage().window().maximize();
